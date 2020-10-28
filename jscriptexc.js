86d3f7343578e74164572651a0969b425c51c82c
facundo chiesa <facundochiesa92@gmail.com>
2020-10-28 03:42:26 -0300

    excercise for completed

--- jscriptexc.js
+++ jscriptexc.js
@@ -109,24 +109,95 @@ var mayNovember = months.slice(4,11);
 
 
 /****If Else****/ /*
-Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”
-Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
-“Bebe” si la edad es menor a 2 años
-“Nino” si la edad es entre 2 y 12 años
-“Adolecente” si la edad es entre 13 y 19 años
-“Joven” si la edad está entre 20 y 30 años
-“Adulto” entre 31 y 60 años
-“Adulto mayor” entre 61 y 75 años
-“Anciano” si es mayor a 75 años
-/****For****/ /*
-Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
-Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
-Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
-Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final (utilizar console.log).
-Funciones
-Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
-A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
-Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
-A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
-Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
+/*A) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor
+ es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un 
+ alerta con el mensaje “Lower than 0,5”*/
+ var number = math.random()
+ if (number >= 0,5){
+ //console.log("Greater than 0,5")
+ }
+else {
+//console.log("Lower than 0,5")
+} 
+
+/*B) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+  “Bebe” si la edad es menor a 2 años
+  “Nino” si la edad es entre 2 y 12 años
+  “Adolecente” si la edad es entre 13 y 19 años
+  “Joven” si la edad está entre 20 y 30 años
+  “Adulto” entre 31 y 60 años
+  “Adulto mayor” entre 61 y 75 años
+  “Anciano” si es mayor a 75 años
 */
+var myAge = 10
+if (myAge < 2){
+    //console.log("Bebe")
+} else if (myAge <= 12) {
+    //console.log("Nino")
+} else if (myAge <= 19) {
+    //console.log("Adolocente")
+} else if (myAge <= 30) {
+    //console.log("Joven")
+} else if (myAge <=60) {
+    //console.log("Adulto")
+} else if (myAge <=75) {
+    //console.log("Adulto mayor")
+} else {  
+    //console.log("Anciano") 
+}
+
+/****For****/ 
+
+/*A) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de 
+JavaScript para mostrar una alerta utilizando cada una de las palabras.*/
+var europe = ["england", "italy", "france", "spain", "germany"]
+for (var i = o; i < europe.length: i++){
+ //alert(europe[i])
+}
+
+/*B) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una 
+alerta por cada palabra modificada.*/
+for (var i = 0; i < europe.length; i++) {
+    europe[i] = europe[i].substring(0,1).toUpperCase() + europe[i].substring(1);
+}
+//alert(europe[i])
+
+
+/*C) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto 
+a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. 
+Al final mostrar una única alerta con la cadena completa.*/
+var europe = ["england", "italy", "france", "spain", "germany"];
+var sentence = [];
+for (var i = o; i < europe.length: i++){
+    sentence.push(europe[i]);
+}
+//alert(sentence)
+
+
+/*D) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número 
+de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos 
+dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al 
+array final (utilizar console.log).*/
+var carp = []
+for (i = 0; i < 10; i++){
+    carp.push(i);
+}
+//console.log(carp)
+
+/****Funciones****/
+/*A) Crear una función suma que reciba dos valores numéricos y retorne el resultado. 
+Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha 
+variable en la consola del navegador.*/
+
+/*B) A la función suma anterior, agregarle una validación para controlar si alguno de los 
+parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error
+ y retornar el valor NaN como resultado.*/
+
+/*C) Crear una función validate integer que reciba un número como parámetro y devuelva verdadero
+ si es un número entero.*/
+
+/*D) A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. 
+En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).*/
+
+/*E) Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función 
+suma probando que todo siga funcionando igual.*/
