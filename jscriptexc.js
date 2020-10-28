3e8f2367dcf1676db96e58395027371e8511980b
facundo chiesa <facundochiesa92@gmail.com>
2020-10-28 04:14:49 -0300

    excercise function added

--- jscriptexc.js
+++ jscriptexc.js
@@ -185,19 +185,35 @@ for (i = 0; i < 10; i++){
 //console.log(carp)
 
 /****Funciones****/
+
 /*A) Crear una función suma que reciba dos valores numéricos y retorne el resultado. 
 Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha 
 variable en la consola del navegador.*/
+function valores (a,b){
+    return a + b;
+}
+//console.log(suma(50,1oo))
 
 /*B) A la función suma anterior, agregarle una validación para controlar si alguno de los 
 parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error
  y retornar el valor NaN como resultado.*/
+//
 
 /*C) Crear una función validate integer que reciba un número como parámetro y devuelva verdadero
  si es un número entero.*/
+ function validateInteger(ab) {
+    if (Number.isInteger(ab)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//console.log(validateInteger(15.20));
 
 /*D) A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. 
 En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).*/
+//
 
 /*E) Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función 
 suma probando que todo siga funcionando igual.*/
+//
